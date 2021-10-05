797aec072c4a2955783f98c96f6ab95b8bc55908
smj-edison <stephen.mason.jones@gmail.com>
2021-10-04 17:05:31 -0700

    Test filter

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use engine::config::SynthConfig;
 use engine::node::Node;
 use engine::node::oscillator::SawOscillatorNode;
 use engine::node::envelope::Envelope;
+use engine::node::filter::{Filter, FilterType};
 use engine::backend::{AudioClientBackend, pulse::PulseClientBackend};
 
 //use engine::backend::
@@ -22,7 +23,7 @@ fn create_test_envelope() -> Envelope {
     Envelope::new(
         0.01,
         0.3,
-        0.3,
+        1.0,
         1.0
     )
 }
@@ -31,7 +32,11 @@ fn create_test_oscillator() -> SawOscillatorNode {
     SawOscillatorNode::new()
 }
 
-fn one_sample(envelope: &mut Envelope, osc: &mut SawOscillatorNode, synth_config: &SynthConfig, gate: f32) -> [f32; BUFFER_SIZE] {
+fn create_test_filter(synth_config: &SynthConfig) -> Filter {
+    Filter::new(FilterType::Lowpass, 1000.0, 5.0, 1.0, synth_config)
+}
+
+fn one_sample(envelope: &mut Envelope, osc: &mut SawOscillatorNode, filter: &mut Filter, synth_config: &SynthConfig, gate: f32) -> [f32; BUFFER_SIZE] {
     osc.process(&synth_config);
 
     let mut OOEI = osc.map_outputs(&synth_config); // oscillator out envelope in
@@ -41,9 +46,14 @@ fn one_sample(envelope: &mut Envelope, osc: &mut SawOscillatorNode, synth_config
     envelope.map_inputs(&OOEI, &synth_config);
     envelope.process(&synth_config);
 
+    let EOFI = envelope.map_outputs(&synth_config); // envelope out filter in
+
+    filter.map_inputs(&EOFI, &synth_config);
+    filter.process(&synth_config);
+
     let mut arr_out = [0_f32; BUFFER_SIZE];
 
-    arr_out.clone_from(envelope.map_outputs(&synth_config).get(&String::from("out")).unwrap());
+    arr_out.clone_from(filter.map_outputs(&synth_config).get(&String::from("out")).unwrap());
 
     arr_out
 }
@@ -51,22 +61,23 @@ fn one_sample(envelope: &mut Envelope, osc: &mut SawOscillatorNode, synth_config
 fn main() {
     let backend = connect_backend();
 
-    let mut osc = create_test_oscillator();
-    let mut envelope = create_test_envelope();
-
     let synth_config = SynthConfig {
         samples_per_second: 48000
     };
 
-    let test = one_sample(&mut envelope, &mut osc, &synth_config, 1.0);
+    let mut osc = create_test_oscillator();
+    let mut envelope = create_test_envelope();
+    let mut filter = create_test_filter(&synth_config);    
 
     for i in 0..3 {
-        let test = one_sample(&mut envelope, &mut osc, &synth_config, 1.0);
+        let test = one_sample(&mut envelope, &mut osc, &mut filter, &synth_config, 1.0);
         backend.write(&test);
     }
 
     loop {
-        let test = one_sample(&mut envelope, &mut osc, &synth_config, 1.0);
+        let test = one_sample(&mut envelope, &mut osc, &mut filter, &synth_config, 1.0);
         backend.write(&test);
+        
+        thread::sleep(Duration::from_millis(((BUFFER_SIZE as u32) / synth_config.samples_per_second).into()));
     }
 }
