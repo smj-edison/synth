f55090c7b892acde6e86cd40b123aea9f5655170
smj-edison <stephen.mason.jones@gmail.com>
2021-10-21 14:04:38 -0700

    Update dummy.rs to new node type

--- engine/src/node/dummy.rs
+++ engine/src/node/dummy.rs
@@ -1,51 +1,37 @@
-use std::collections::HashMap;
-
-use crate::constants::{BUFFER_SIZE};
-
-use crate::node::Node;
+use crate::node::{Node, InputType, OutputType};
 
 pub struct Dummy {
-    input_in: [f64; BUFFER_SIZE],
-    output_out: [f64; BUFFER_SIZE]
+    input_in: f64,
+    output_out: f64
 }
 
 impl Dummy {
     pub fn new() -> Dummy {
         Dummy {
-            input_in: [0_f64; BUFFER_SIZE],
-            output_out: [0_f64; BUFFER_SIZE]
+            input_in: 0_f64,
+            output_out: 0_f64
         }
     }
 
-    pub fn set_output_out(&mut self, output_out: [f64; BUFFER_SIZE]) {
+    pub fn set_output_out(&mut self, output_out: f64) {
         self.output_out = output_out;
     }
 
-    pub fn get_input_in(&self) -> &[f64; BUFFER_SIZE] {
-        &self.input_in
+    pub fn get_input_in(&self) -> f64 {
+        self.input_in
     }
 }
 
 impl Node for Dummy {
-    fn map_inputs(&mut self, buffers: &HashMap<String, [f64; BUFFER_SIZE]>) {
-        let buffer_in = match buffers.get(&String::from("out")) {
-            Some(gate) => &gate,
-            None => &[0_f64; BUFFER_SIZE]
-        };
-
-        self.input_in.clone_from(buffer_in);
+    fn receive_audio(&mut self, input_type: InputType, input: f64) {
+        self.input_in = input;
     }
 
     fn process(&mut self) {
         
     }
 
-    fn map_outputs(&self) -> HashMap<String, [f64; BUFFER_SIZE]> {
-        let mut outputs:HashMap::<String, [f64; BUFFER_SIZE]> = HashMap::new();
-        
-        outputs.insert(String::from("out"), self.output_out);
-        
-        //outputs
-        outputs
+    fn get_output_audio(&self, output_type: OutputType) -> f64 {
+        self.output_out
     }
 }
