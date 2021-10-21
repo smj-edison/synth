3e44fb7083e68d2d93400624eead4829e5d7f5f9
smj-edison <stephen.mason.jones@gmail.com>
2021-10-21 14:42:18 -0700

    Fix audio IO

--- engine/src/node/dummy.rs
+++ engine/src/node/dummy.rs
@@ -24,7 +24,10 @@ impl Dummy {
 
 impl Node for Dummy {
     fn receive_audio(&mut self, input_type: InputType, input: f64) {
-        self.input_in = input;
+        match input_type {
+            In => self.input_in = input,
+            _ => panic!("Cannot receive {:?}", input_type)
+        }
     }
 
     fn process(&mut self) {
@@ -32,6 +35,9 @@ impl Node for Dummy {
     }
 
     fn get_output_audio(&self, output_type: OutputType) -> f64 {
-        self.output_out
+        match output_type {
+            Out => self.output_out,
+            _ => panic!("Cannot output {:?}", output_type)
+        }
     }
 }
