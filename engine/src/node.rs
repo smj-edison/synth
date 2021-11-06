899c4a5d642823775b7ae2bdf06de3980090fbad
smj-edison <stephen.mason.jones@gmail.com>
2021-11-06 14:16:26 -0700

    Add midi node

--- engine/src/node.rs
+++ engine/src/node.rs
@@ -6,11 +6,16 @@ pub mod oscillator;
 pub mod ramp;
 
 use simple_error::SimpleError;
+use crate::midi::messages::MidiData;
 
 pub trait AudioNode {
     fn process(&mut self);
     fn receive_audio(&mut self, input_type: InputType, input: f32) -> Result<(), SimpleError>;
     fn get_output_audio(&self, output_type: OutputType) -> Result<f32, SimpleError>;
+}
+
+pub trait MidiNode {
+    fn receive_midi(&mut self, input_type: InputType, input: &Vec<MidiData>) -> Result<(), SimpleError>;
 }
 
 #[derive(Debug)]
